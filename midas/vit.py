18cb2e33817bf2390725c14556576a1f3303bb58
Noah Cohn <noahc42@gmail.com>
2022-08-26 05:05:52 -0700

    Update vit.py

--- midas/vit.py
+++ midas/vit.py
@@ -14,7 +14,6 @@ class Slice(nn.Module):
     def forward(self, x):
         return x[:, self.start_index :]
 
-
 class AddReadout(nn.Module):
     def __init__(self, start_index=1):
         super(AddReadout, self).__init__()
@@ -27,47 +26,36 @@ class AddReadout(nn.Module):
             readout = x[:, 0]
         return x[:, self.start_index :] + readout.unsqueeze(1)
 
-
 class ProjectReadout(nn.Module):
     def __init__(self, in_features, start_index=1):
         super(ProjectReadout, self).__init__()
         self.start_index = start_index
-
         self.project = nn.Sequential(nn.Linear(2 * in_features, in_features), nn.GELU())
-
     def forward(self, x):
         readout = x[:, 0].unsqueeze(1).expand_as(x[:, self.start_index :])
         features = torch.cat((x[:, self.start_index :], readout), -1)
-
         return self.project(features)
-
 
 class Transpose(nn.Module):
     def __init__(self, dim0, dim1):
         super(Transpose, self).__init__()
         self.dim0 = dim0
         self.dim1 = dim1
-
     def forward(self, x):
         x = x.transpose(self.dim0, self.dim1)
         return x
 
-
 def forward_vit(pretrained, x):
     b, c, h, w = x.shape
-
     glob = pretrained.model.forward_flex(x)
-
     layer_1 = pretrained.activations["1"]
     layer_2 = pretrained.activations["2"]
     layer_3 = pretrained.activations["3"]
     layer_4 = pretrained.activations["4"]
-
     layer_1 = pretrained.act_postprocess1[0:2](layer_1)
     layer_2 = pretrained.act_postprocess2[0:2](layer_2)
     layer_3 = pretrained.act_postprocess3[0:2](layer_3)
     layer_4 = pretrained.act_postprocess4[0:2](layer_4)
-
     unflatten = nn.Sequential(
         nn.Unflatten(
             2,
@@ -79,7 +67,6 @@ def forward_vit(pretrained, x):
             ),
         )
     )
-
     if layer_1.ndim == 3:
         layer_1 = unflatten(layer_1)
     if layer_2.ndim == 3:
@@ -88,46 +75,35 @@ def forward_vit(pretrained, x):
         layer_3 = unflatten(layer_3)
     if layer_4.ndim == 3:
         layer_4 = unflatten(layer_4)
-
     layer_1 = pretrained.act_postprocess1[3 : len(pretrained.act_postprocess1)](layer_1)
     layer_2 = pretrained.act_postprocess2[3 : len(pretrained.act_postprocess2)](layer_2)
     layer_3 = pretrained.act_postprocess3[3 : len(pretrained.act_postprocess3)](layer_3)
     layer_4 = pretrained.act_postprocess4[3 : len(pretrained.act_postprocess4)](layer_4)
-
     return layer_1, layer_2, layer_3, layer_4
-
 
 def _resize_pos_embed(self, posemb, gs_h, gs_w):
     posemb_tok, posemb_grid = (
         posemb[:, : self.start_index],
         posemb[0, self.start_index :],
     )
-
     gs_old = int(math.sqrt(len(posemb_grid)))
-
     posemb_grid = posemb_grid.reshape(1, gs_old, gs_old, -1).permute(0, 3, 1, 2)
     posemb_grid = F.interpolate(posemb_grid, size=(gs_h, gs_w), mode="bilinear")
     posemb_grid = posemb_grid.permute(0, 2, 3, 1).reshape(1, gs_h * gs_w, -1)
-
     posemb = torch.cat([posemb_tok, posemb_grid], dim=1)
-
     return posemb
-
 
 def forward_flex(self, x):
     b, c, h, w = x.shape
-
     pos_embed = self._resize_pos_embed(
         self.pos_embed, h // self.patch_size[1], w // self.patch_size[0]
     )
-
     B = x.shape[0]
 
     if hasattr(self.patch_embed, "backbone"):
         x = self.patch_embed.backbone(x)
         if isinstance(x, (list, tuple)):
             x = x[-1]  # last feature if backbone outputs list/tuple of features
-
     x = self.patch_embed.proj(x).flatten(2).transpose(1, 2)
 
     if getattr(self, "dist_token", None) is not None:

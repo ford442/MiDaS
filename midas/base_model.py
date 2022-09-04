222defd8207b4844d08dff8979f50baca26670b0
Noah Cohn <noahc42@gmail.com>
2022-09-04 04:14:36 -0700

    Update base_model.py

--- midas/base_model.py
+++ midas/base_model.py
@@ -2,7 +2,7 @@ import torch
 class BaseModel(torch.nn.Module):
     def load(self, path):
         #parameters = torch.load(path,map_location=lambda storage,loc:storage.cuda(0))
-        parameters = torch.load(path,map_location=torch.device("cpu"))
+        parameters = torch.load(path,map_location=torch.cuda.device("cuda:0"))
         if "optimizer" in parameters:
             parameters = parameters["model"]
         self.load_state_dict(parameters)
